47e8cc4a1713c4ae4788fe715c24c7287e65dfdd
Benjamin Lorenz <lorenz@math.tu-berlin.de>
2020-07-01 15:38:25 +0200

    add another small patch for cross compilation
    and reorganize patches slightly

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -3,7 +3,7 @@
 using BinaryBuilder, Pkg
 
 name = "polymake"
-version = v"4.1"
+version = v"4.1.1"
 
 # Collection of sources required to build polymake
 sources = [
@@ -30,6 +30,7 @@ if [[ $target == *darwin* ]]; then
   # for a modified pure perl JSON module and to make miniperl find the correct modules
   export PERL5LIB=$prefix/lib/perl5/$perl_version:$prefix/lib/perl5/$perl_version/darwin-2level:$WORKSPACE/srcdir/patches
   atomic_patch -p1 ../patches/polymake-cross.patch
+  atomic_patch -p1 ../patches/polymake-cross-build.patch
 else
   ./configure CFLAGS="-Wno-error" CC="$CC" CXX="$CXX" \
               PERL=${prefix}/bin/perl LDFLAGS="$LDFLAGS" \
@@ -52,7 +53,7 @@ ninja -v -C build/Opt install
 
 # undo patch needed for building
 if [[ $target == *darwin* ]]; then
-  atomic_patch -R -p1 ../patches/polymake-cross.patch
+  atomic_patch -R -p1 ../patches/polymake-cross-build.patch
 fi
 install -m 444 -D support/*.pl $prefix/share/polymake/support/
 
@@ -71,9 +72,8 @@ install_license COPYING
 # platforms are passed in on the command line
 platforms = expand_cxxstring_abis([
  MacOS(:x86_64)
- Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(libstdcxx_version = v"3.4.23", cxxstring_abi=:cxx11))
- Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(libstdcxx_version = v"3.4.25", cxxstring_abi=:cxx11))
- Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(libstdcxx_version = v"3.4.26", cxxstring_abi=:cxx11))
+ Linux(:x86_64, libc=:glibc)
+ Linux(:i686, libc=:glibc)
 ])
 
 # The products that we will ensure are always built
@@ -85,22 +85,17 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    # upstream
-    Dependency(PackageSpec(name="GMP_jll", uuid="781609d7-10c4-51f6-84f2-b8444358ff6d"))
-    Dependency(PackageSpec(name="MPFR_jll", uuid="3a97d323-0669-5f0c-9066-3539efd106a3"))
-    Dependency(PackageSpec(name="boost_jll", uuid="28df3c45-c428-5900-9ff8-a3135698ca75"))
-    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))
-    Dependency(PackageSpec(name="lrslib_jll", uuid="3873f7d0-7b7c-52c3-bdf4-8ab39b8f337a"))
-    Dependency(PackageSpec(name="PPL_jll", uuid="80dd9cbb-8b87-5171-a280-372cc418f402"))
-    Dependency(PackageSpec(name="cddlib_jll", uuid="f07e07eb-5685-515a-97c8-3014f6152feb"))
-    Dependency(PackageSpec(name="FLINT_jll", uuid="e134572f-a0d5-539d-bddf-3cad8db41a82",version=v"0.0.2"))
-    # normaliz fixed to 3.8.4 until switch to flint 2.6
-    Dependency(PackageSpec(name="normaliz_jll", uuid="6690c6e9-4e12-53b8-b8fd-4bffaef8839f", version=v"3.8.4"))
-    Dependency(PackageSpec(name="bliss_jll", uuid="508c9074-7a14-5c94-9582-3d4bc1871065"))
-
-    # local
-    Dependency(PackageSpec(name="Perl_jll", uuid="83958c19-0796-5285-893e-a1267f8ec499",url="https://github.com/benlorenz/Perl_jll.jl"))
-    #, version=v"5.30.3"))
+    Dependency(PackageSpec(name="Perl_jll", uuid="83958c19-0796-5285-893e-a1267f8ec499", version=v"5.30.3"))
+    Dependency("GMP_jll")
+    Dependency("MPFR_jll")
+    Dependency("boost_jll")
+    Dependency("CompilerSupportLibraries_jll")
+    Dependency("lrslib_jll")
+    Dependency("PPL_jll")
+    Dependency("cddlib_jll")
+    Dependency("bliss_jll")
+    Dependency(PackageSpec(name="FLINT_jll", uuid="e134572f-a0d5-539d-bddf-3cad8db41a82",version=v"2.6.0"))
+    Dependency("normaliz_jll")
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
